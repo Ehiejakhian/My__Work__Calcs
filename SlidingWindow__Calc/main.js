dbec3a5bdad3e48737f9b3d26f07e55a6df81775
Ehi Ejakhian <ehiejakhian@gmail.com>
2026-02-12 22:22:56 +0100

    replaced sliding window stuff with new files

--- SlidingWindow__Calc/main.js
+++ SlidingWindow__Calc/main.js
@@ -39,3 +39,47 @@ function display() {
   document.querySelector('.glass-height-output').textContent = glassHeight;
   
 }
+
+let w_err = document.querySelector('.widthError');
+let h_err = document.querySelector('.heightError');
+setInterval(()=>{
+  error();
+}, 100);
+
+
+function error() {
+  width.addEventListener('keyup', ()=>{
+    if (!width.value) {
+      width.classList.add('error');
+      w_err.textContent = 'Empty?'
+    } else {
+      if (width.value.length < 3) {
+        width.classList.add('error');
+        w_err.textContent = 'Is\'nt this too small?';
+      } else if(width.value > 16000) {
+        width.classList.add('error');
+        w_err.textContent = 'That\'s way large than what we have (16,000 mm)';
+      } else {
+        width.classList.remove('error');
+        w_err.textContent = '';
+      }
+    }
+  })
+  height.addEventListener('keyup', ()=>{
+    if (!height.value) {
+      height.classList.add('error');
+      h_err.textContent = 'Empty?'
+    } else {
+      if (height.value.length < 3) {
+        height.classList.add('error');
+        h_err.textContent = 'Is\'nt this too small?';
+      } else if(height.value > 16000) {
+        height.classList.add('error');
+        h_err.textContent = 'That\'s way large than what we have (16,000 mm)';
+      } else {
+        height.classList.remove('error');
+        h_err.textContent = '';
+      }
+    }
+  })
+}
